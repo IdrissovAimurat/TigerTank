f543bfd5bd1a0b1fcec0f7cc5a0fb799ed109b9c
call me ai|sem <aimur349Gmail.com>
2023-10-24 00:15:47 +0600

    я больше не армянин

--- Program.cs
+++ Program.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using MyClassLib.WordOfTanks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TigerTank
 {
-    internal class Program
+    public class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
         static void Main(string[] args)
         {
+            Tank[] T34Tanks = new Tank[5];
 
+            Tank[] PanteraTanks = new Tank[5];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                T34Tanks[i] = new Tank("\nT-34 " + (i + 1), rand.Next(0, 101), rand.Next(0, 101), rand.Next(0, 101));
+
+                PanteraTanks[i] = new Tank("\nPantera " + (i + 1), rand.Next(0, 101), rand.Next(0, 101), rand.Next(0, 101));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"\n{T34Tanks[i]} vs {PanteraTanks[i]}");
+
+                bool T34Wins = T34Tanks[i] ^ PanteraTanks[i];
+
+                Console.WriteLine(T34Wins ? "\nИтог: \tT-34 Пoбедил, ИУУУУУУ" : "\nИтог: \tPantera Победил, Мой Ф***р");
+            }
+            Console.ReadKey();
         }
     }
 }
